a94d1733b8c0550d516aab5baa520c19e8cd33f4
Nikita <30903256+NikitosGolubev@users.noreply.github.com>
2020-04-16 22:45:14 +0300

    4. Методы и строгая типизация - ФУНКЦИЯ РЕАЛИЗОВАНА.

--- app/Article.php
+++ app/Article.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Exceptions\MismatchException;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -14,5 +15,15 @@ class Article extends Model
     public function users() {
         // works convention about 3rd table and connecting field names
         return $this->belongsToMany('App\User');
+    }
+
+    // Я же могу поставить предопределение типа аргумента -> User $user
+    public function isUserAuthor($user) {
+        if (!($user instanceof User)) {
+            throw new MismatchException('Вы можете передвавать в эту функцию только объекты класса User.');
+        }
+
+        if ($this->trashed()) return null; // если статья удалена
+        return $user->articles()->exists($this); //  true если статья пренадлежит пользователю, false, если нет
     }
 }
